2316255afcae7d7ff99856c15c01539f9fbd088e
MUHAMMAD IQBAL <muhammadiqbal2015@hotmail.com>
2018-10-19 02:34:09 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const app = require('./app');
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 // console.log('Listening on port: ', port);
