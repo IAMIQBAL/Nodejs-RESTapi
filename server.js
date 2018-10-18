fe8d6d4a7ce03629a604bcab6451a04333a87915
muhammadiqbal2017 <muhammadiqbal2015@hotmail.com>
2018-10-19 02:07:08 +0500

    .

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const http = require('http');
 const app = require('./app');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 // console.log('Listening on port: ', port);
-server.listen(port);
+server.listen(port, function(){
+    console.log("App is running on port " + port)
+});
